6318add167c90473cb7bc0a05335288edf4bef52
Nikhil-Mindpath <159872995+Nikhil-Mindpath@users.noreply.github.com>
2024-02-13 07:20:02 +0000

    variabels basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 456;  // it is an constant value..
+let mail =  "hitesh@gmail.com";
+var password = "290";
+
+city = "Indore"  //taking space witout declration
+// accountId = 89; // cant modify
+
+console.log(accountId);
+//if we had to print in table
+console.table([accountId,mail,password]);
+
+// Initially scoping was not decided 
+// the problem is to change the variable value based on scoping 
+// after advance versions var let const was arrived  
+
+let balanace;   //shows undefined means  not initialized
