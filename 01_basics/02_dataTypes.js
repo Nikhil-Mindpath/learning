d58acc563d51de9ec3034de025ae3f3b9c71fa02
Nikhil-Mindpath <159872995+Nikhil-Mindpath@users.noreply.github.com>
2024-02-13 07:49:49 +0000

    datatypes basics

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" //it will treat the code as an newer versions now it was done auto 
+
+// alert(3+3) // it willl not be used b
+
+let nuber  = 18 ; // it has capability of 2 pow 53 or use bigInt
+
+// BigInt bigNumber  = 989898;  this is wrong syntax 
+let vari;
+let bigNumber  =  999999999999999999999n;
+console.log(bigNumber);
+
+//undefined  =  not intitialized 
+// symbol =  for uniquesness it is used 
+// null =  standalone value
+//  treates as obj 
+
+console.log(typeof null);
+console.log(typeof vari); // shows undefined
+
