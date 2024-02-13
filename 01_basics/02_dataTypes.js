e768ca6ba1aad8abddf90667520da75505f2d9bc
Nikhil-Mindpath <159872995+Nikhil-Mindpath@users.noreply.github.com>
2024-02-13 10:06:53 +0000

    operator and operations in js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -7,19 +7,28 @@ let nuber  = 18 ; // it has capability of 2 pow 53 or use bigInt
 // BigInt bigNumber  = 989898;  this is wrong syntax 
 let vari;
 let bigNumber  =  999999999999999999999n;
-console.log(bigNumber);
+// console.log(bigNumber);
 
 //undefined  =  not intitialized 
 // symbol =  for uniquesness it is used 
 // null =  standalone value
 //  treates as obj 
 
-console.log(typeof null);
-console.log(typeof vari); // shows undefined
+// console.log(typeof null);
+// console.log(typeof vari); // shows undefined
 
 
 // *********** Operations  *************** 
 
-let value  = 3;
+let value  = "3";
+console.log(typeof value);
+let negValue   =  -value;
+console.log(negValue);
+console.log(typeof negValue); // it will be number type now 
 
 
+let boolValue  = +true;
+console.log(boolValue); // 
+console.log(typeof boolValue);
+
+// in javascrpt < > <= >= works differently 
